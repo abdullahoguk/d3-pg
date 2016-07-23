df1bb1e75fa42d101de4e57298fbf5dd1d1d452e
Abdullah Öğük <abdullahoguk@gmail.com>
2016-07-23 22:17:37 +0300

    -

--- weather-history/js/main.js
+++ weather-history/js/main.js
@@ -1,1 +1,33 @@
-var getE
+//python -m SimpleHTTPServer 8000
+
+$(document).ready(function(){
+
+//https://raw.githubusercontent.com/codeaia/chart-ui/gh-pages/data1.json
+//http://localhost:8000/data1.json 
+	d3.json("https://raw.githubusercontent.com/abdullahoguk/d3-pg/master/weather-history/weather.json", 
+		function(error, data){
+			if (error) return console.warn(error);
+  			d=data;
+  			height = 500;
+  			width = 700;
+  			padding = 100;
+
+
+
+  			
+  			var wholesvg = d3.select("#chart1").append("svg")
+  				.attr("height", height+padding*2)
+  				.attr("width",width+padding*2)
+  				.attr("class","chartsvg")
+  				.call(zoom);
+
+  				var chartsvg = wholesvg.append('g')
+  				.attr("class","chart")
+  				.attr('transform', 'translate(' + padding + ',' + padding + ')')
+  			
+  			
+
+			
+
+  		})//end of callback function
+})
