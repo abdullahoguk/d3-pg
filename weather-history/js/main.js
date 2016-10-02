820a8f6533530a57db7a89c9331c009072a2b150
Abdullah Öğük <abdullahoguk@gmail.com>
2016-10-02 22:17:40 +0300

    2

--- weather-history/js/main.js
+++ weather-history/js/main.js
@@ -17,9 +17,9 @@ $(document).ready(function(){
 		"minT":""
 	}
 ]
-
 */
-	d3.json("https://raw.githubusercontent.com/abdullahoguk/d3-pg/master/weather-history/weather.json",
+//https://raw.githubusercontent.com/abdullahoguk/d3-pg/master/weather-history/weather.json
+	d3.json("http://localhost:8000/weather.json",
 		function(error, data){
 			if (error) return console.warn(error);
 
@@ -38,7 +38,8 @@ $(document).ready(function(){
 			innerTickSize = 5;
 			outerTickSize = 2;
 
-
+			maxDays = 8;
+			minDays = 4;
 
 			//Scales
 			tempSc = d3.scale.linear()
@@ -64,28 +65,26 @@ $(document).ready(function(){
 			var wholesvg = d3.select("#chart1").append("svg")
 				.attr("height", height+padding*2)
 				.attr("width",width+padding*2)
-				.attr("class","chartsvg")
+				.attr("class","chartsvg");
+
 
 			var chartsvg = wholesvg.append('g')
 				.attr("class","chart")
 				.attr('transform', 'translate(' + padding + ',' + padding + ')');
 
 
-/*
+
+
 			//LINES
 				line = d3.svg.line()
 					.x(function(d) {return timeSc(new Date(d.date)); })
 					.y(function(d) {return tempSc(d.meanT)});
-
-
 				var lines = chartsvg.append("path")
 					.data([d])
 					.attr("class", "line")
 					.attr("d", line(d) )
-					.attr("transform", "translate(40,0)")
+					.attr("transform", "translate(0,0)")
 
-
-*/
 				//DOTS GROUP(each of them contains a circle and text)
 
 
@@ -94,9 +93,10 @@ $(document).ready(function(){
 						.enter()
 						.append("g")
 						.attr("class","dotsg")
-						.attr("transform", "translate(0,0)")
+						.attr("transform", "translate(0,0)");
 
-						dotsg.append("circle")
+
+					var dots = dotsg.append("circle")
 							.attr("class","dots")
 							.attr("cx", function(d){
 								return timeSc(new Date(d.date))
@@ -137,6 +137,174 @@ $(document).ready(function(){
 					.attr("class" , "yaxis")
 					.attr("transform", "translate(0, 0)")
 					.call(yAxis)
+
+
+//-------------------------------------------------------NEW
+
+
+var area = d3.svg.area()
+    .interpolate("step-after")
+    .x(function(d) { return timeSc(new Date(d.date)); })
+    .y0(tempSc(0))
+    .y1(function(d) { return tempSc(d.meanT); });
+
+pane = chartsvg.append("rect")
+	.attr("class", "pane")
+	.attr("width", width)
+	.attr("height", height)
+	.call(
+		d3.behavior.zoom()
+		.x(timeSc)
+		.scaleExtent([1, 10])
+		.on("zoom", function(){
+			chartsvg.attr("transform","translate("
+		+ d3.event.translate
+		+ ")scale(" + d3.event.scale + ")");
+		chartsvg.select("g.x.axis").call(xAxis);
+	  	chartsvg.select("g.y.axis").call(yAxis);
+		chartsvg.select("path.line").attr("d", line);
+		chartsvg.select("path.area").attr("d", area);
+		})
+	);
+
+
+
+
+var zoom = d3.behavior.zoom()
+	.x(timeSc)
+	.scaleExtent([1,2])
+	.on("zoom", function(){
+/*
+		var t = zoom.translate(),
+	      tx = t[0],
+	      ty = t[1];
+
+		tx = Math.min(1, Math.max(tx, width - Math.round(timeSc(maxDays) - timeSc(1)), width - Math.round(timeSc(maxDays) - timeSc(1)) * d3.event.scale))
+		zoom.translate([tx, ty])
+*/
+
+	chartsvg.attr("transform","translate("
++ d3.event.translate
++ ")scale(" + d3.event.scale + ")");
+
+	  chartsvg.select("g.x.axis").call(xAxis);
+	  chartsvg.select("g.y.axis").call(yAxis);
+	 // chartsvg.select("path.area").attr("d", area);
+	  chartsvg.select("path.line").attr("d", line);
+		//chartsvg.select("circle.dots").attr("d", dots);
+		chartsvg.select(".dotsg").call(dots);
+
+	//draw();
+});
+
+
+//    draw();
+
+
+function draw() {
+	/*
+	var t = zoom.translate(),
+      tx = t[0],
+      ty = t[1];
+
+	tx = Math.min(1, Math.max(tx, width - Math.round(timeSc(maxDays) - timeSc(1)), width - Math.round(timeSc(maxDays) - timeSc(1)) * d3.event.scale))
+	zoom.translate([tx, ty])
+*/
+  chartsvg.select("g.x.axis").call(xAxis);
+  chartsvg.select("g.y.axis").call(yAxis);
+ // chartsvg.select("path.area").attr("d", area);
+  chartsvg.select("path.line").attr("d", line);
+}
+
+/*
+
+var area = d3.svg.area()
+    .interpolate("step-after")
+    .x(function(d) { return timeSc(new Date(d.date)); })
+    .y0(tempSc(0))
+    .y1(function(d) { return tempSc(d.meanT); });
+
+var line = d3.svg.line()
+    .interpolate("step-after")
+    .x(function(d) { return timeSc(new Date(d.date)); })
+    .y(function(d) { return tempSc(d.meanT); });
+
+
+
+
+	var gradient = chartsvg.append("defs").append("linearGradient")
+	    .attr("id", "gradient")
+	    .attr("x2", "0%")
+	    .attr("y2", "100%");
+
+	gradient.append("stop")
+	    .attr("offset", "0%")
+	    .attr("stop-color", "#fff")
+	    .attr("stop-opacity", .5);
+
+	gradient.append("stop")
+	    .attr("offset", "100%")
+	    .attr("stop-color", "#999")
+	    .attr("stop-opacity", 1);
+
+
+var xAxis = d3.svg.axis()
+    .scale(timeSc)
+    .orient("bottom")
+	.ticks(d.length/100)
+    .tickSize(innerTickSize,outerTickSize)
+    .tickPadding(6);
+
+var yAxis = d3.svg.axis()
+    .scale(tempSc)
+    .orient("left")
+	.ticks(10)
+    .tickSize(innerTickSize,outerTickSize)
+    .tickPadding(6);
+
+
+
+
+	chartsvg.append("g")
+	    .attr("class", "yaxis")
+	    .attr("transform", "translate(" + 0 + ",0)");
+
+	chartsvg.append("path")
+	    .attr("class", "area")
+	    .attr("clip-path", "url(#clip)")
+	    .style("fill", "url(#gradient)");
+
+	chartsvg.append("g")
+	    .attr("class", "xaxis")
+	    .attr("transform", "translate(0," + height + ")");
+
+	chartsvg.append("path")
+	    .attr("class", "line")
+	    .attr("clip-path", "url(#clip)");
+
+	chartsvg.append("rect")
+	    .attr("class", "pane")
+	    .attr("width", width)
+	    .attr("height", height)
+	    .call(zoom);
+
+
+
+
+  chartsvg.select("path.area").data([data]);
+  chartsvg.select("path.line").data([data]);
+  draw();
+
+
+function draw() {
+  chartsvg.select("g.x.axis").call(xAxis);
+  chartsvg.select("g.y.axis").call(yAxis);
+  chartsvg.select("path.area").attr("d", area);
+  chartsvg.select("path.line").attr("d", line);
+}
+*/
+//-------------------------------------------------------------NEW
+
 
 
 
